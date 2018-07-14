0b94a7746afc1c3dd13ba9d7429b9348b209a449
cj96248 <253037585@qq.com>
2018-07-14 23:26:33 +0800

    remove non-used code

--- src/main/java/org/wltea/analyzer/query/IKQueryExpressionParser.java
+++ src/main/java/org/wltea/analyzer/query/IKQueryExpressionParser.java
@@ -704,13 +704,4 @@ public class IKQueryExpressionParser {
 		}
 	}	
 
-	public static void main(String[] args){
-		IKQueryExpressionParser parser = new IKQueryExpressionParser();
-		//String ikQueryExp = "newsTitle:'的两款《魔兽世界》插件Bigfoot和月光宝盒'";
-		String ikQueryExp = "(id='ABcdRf' && date:{'20010101','20110101'} && keyword:'魔兽中国') || (content:'KSHT-KSH-A001-18'  || ulr='www.ik.com') - name:'林良益'";
-		Query result = parser.parseExp(ikQueryExp , true);
-		System.out.println(result);
-
-	}	
-	
 }
